099dc81112d890eca434aec481cbdce4920c210b
spr0nk6 <paulius.misevicius@protonmail.com>
2026-07-04 12:55:22 +0300

    Add Firebase authentication with email and password

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,22 +1,31 @@
 import { Mail, Lock } from "lucide-react"
+import { addUserToFirebase } from "../utils"
 
 export default function Signup() {
+
+    function createNewAccount(formData) {
+        const email = formData.get("email")
+        const password = formData.get("password")
+
+        addUserToFirebase(email, password)
+    }
+
     return (
         <section className="sign-up">
-            <form className="sign-up_form">
+            <form action={createNewAccount} className="sign-up_form">
                 <div className="sign-up_fields">
                     <div className="sign-up_field">
                         <label htmlFor="email">Email</label>
                         <div className="input-container">
                             <Mail className="input-icon sign-up_mail-icon" />
-                            <input id="email" type="email" />
+                            <input required name="email" id="email" type="email" />
                         </div>
                     </div>
                     <div className="sign-up_field">
                         <label htmlFor="password">Password</label>
                         <div className="input-container">
                             <Lock className="input-icon sign-up_password-icon" />
-                            <input id="password" type="password" />
+                            <input required name="password" id="password" type="password" />
                         </div>
                     </div>
                 </div>
